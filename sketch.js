e26ab653a59273388c485c611a3c7c846118674b
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-02-02 04:56:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,11 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint=Matter.Constraint;
-var treeObj, stoneObj,groundObject, launcherObject;
-var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
-var world,boy;
+
+
+//create given objects
+
+
 var launchingForce=100;
 
 function preload(){
@@ -18,26 +20,10 @@ function setup() {
 	createCanvas(1300, 600);
 	engine = Engine.create();
 	world = engine.world;
-
-	stoneObj=new stone(235,420,30); 
-
-	mango1=new mango(1100,100,30);
-  mango2=new mango(1170,130,30);
-	mango3=new mango(1010,140,30);
-	mango4=new mango(1000,70,30);
-	mango5=new mango(1100,70,30);
-	mango6=new mango(1000,230,30);
-	mango7=new mango(900,230,40);
-	mango8=new mango(1140,150,40);
-	mango9=new mango(1100,230,40);
-	mango10=new mango(1200,200,40);
-	mango11=new mango(1120,50,40);
-	mango12=new mango(900,160,40);
-
-	treeObj=new tree(1050,580);
-	groundObject=new ground(width/2,600,width,20);
+//create objects
 	
-	//create launcherObject and assign points 
+	
+	
 	
 	
   var render = Render.create({
@@ -51,37 +37,24 @@ function setup() {
   });
 	
 	Engine.run(engine);
- // Render.run(render);
+
 }
 
 function draw() {
 
   background(230);
-  //frameRate(2)
- // Engine.update(engine)
-  textSize(25);
-  text("Press Space to get a second Chance to Play!!",50 ,50);
-  image(boy ,200,340,200,300);
-  //Engine.update(engine)
+
+ //add text
+	
+	
+
   
 
-  treeObj.display();
-  stoneObj.display();
-  mango1.display();
-  mango2.display();
-  mango3.display();
-  mango4.display();
-  mango6.display();
- mango7.display();
-  mango8.display();
-  mango9.display();
-  mango10.display();
-  mango11.display();
-  mango12.display();
-  stoneObj.display();
-
-  groundObject.display();
-  launcherObject.display();
+//display the objects
+	
+	
+	
+	
   detectollision(stoneObj,mango1);
   detectollision(stoneObj,mango2);
   detectollision(stoneObj,mango3);
