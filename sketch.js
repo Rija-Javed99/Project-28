d8c6cc8f5fe5870a1e00d3cf5a6ce4503fcea13b
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-18 15:44:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,9 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint=Matter.Constraint;
-var treeObj, stoneObj,groundObject, launcherObject;
-var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
+//var of ojects
+
+
 var world,boy;
 var launchingForce=100;
 
@@ -14,7 +15,7 @@ var launchingForce=100;
 
 
 function preload(){
-	boy=loadImage("images/boy.png");
+	//load boy image
   }
 
 
@@ -25,25 +26,11 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	stoneObj=new stone(235,420,30); 
-
-	mango1=new mango(1100,100,30);
-        mango2=new mango(1170,130,30);
-	mango3=new mango(1010,140,30);
-	mango4=new mango(1000,70,30);
-	mango5=new mango(1100,70,30);
-	mango6=new mango(1000,230,30);
-	mango7=new mango(900,230,25);
-	mango8=new mango(1140,150,25);
-	mango9=new mango(1100,230,25);
-	mango10=new mango(1200,200,25);
-	mango11=new mango(1120,50,25);
-	mango12=new mango(900,160,25);
-
-	treeObj=new tree(1050,580);
-	groundObject=new ground(width/2,600,width,20);
-	launcherObject=new launcher(stoneObj.body,{x:235,y:420})
- 
+	//create all objects
+	
+	
+	
+	
 	Engine.run(engine);
  
 }
@@ -59,23 +46,9 @@ function draw() {
   text("Press Space to get a second Chance to Play!!",50 ,50);
   image(boy ,200,340,200,300);
 
-  treeObj.display();
-  stoneObj.display();
-  mango1.display();
-  mango2.display();
-  mango3.display();
-  mango4.display();
-  mango6.display();
-  mango7.display();
-  mango8.display();
-  mango9.display();
-  mango10.display();
-  mango11.display();
-  mango12.display();
-  stoneObj.display();
-
-  groundObject.display();
-  launcherObject.display();
+  //display the objects
+	
+	
   detectollision(stoneObj,mango1);
   detectollision(stoneObj,mango2);
   detectollision(stoneObj,mango3);
@@ -110,8 +83,7 @@ function mouseReleased()
 
 function keyPressed() {
 	if (keyCode === 32) {
-    Matter.Body.setPosition(stoneObj.body, {x:235, y:420}) 
-	  launcherObject.attach(stoneObj.body);
+    //attach stone back
 	}
   }
 
